5ce3d3ed2b53343dc780b5373f595d8e3786a8ba
Syllayoussouf <youssousylla552@gmail.com>
2025-12-15 20:12:26 +0000

    ✨ F01: LOGIN > Add title & form

--- src/components/pages/LoginPage.jsx
+++ src/components/pages/LoginPage.jsx
@@ -1,8 +1,35 @@
+import { useState } from "react";
+
 export default function LoginPage() {
   //state
+  const [inputValue, setInputValue] = useState("");
 
   //comportement
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    alert(`Bonjour ${inputValue}`);
+    setInputValue("");
+  };
+  const handlechange = (event) => {
+    setInputValue(event.target.value);
+  };
 
   //render
-  return <div>LoginPage</div>;
+  return (
+    <div>
+      <h1>Bienvenue chez nous !</h1>
+      <br />
+      <h2>Connectez-vous</h2>
+      <form action="submit" onSubmit={handleSubmit}>
+        <input
+          type="text"
+          placeholder="Entrer votre prénom..."
+          required
+          value={inputValue}
+          onChange={handlechange}
+        />
+        <button>Accéder à votre espace</button>
+      </form>
+    </div>
+  );
 }
