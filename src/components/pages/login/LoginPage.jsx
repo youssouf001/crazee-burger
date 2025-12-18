91003c26be29eda4117eb9c15fa9779d1e722be8
Syllayoussouf <youssousylla552@gmail.com>
2025-12-18 13:38:25 +0000

    💄 F03 : LOGIN > Add styling and background

--- src/components/pages/login/LoginPage.jsx
+++ src/components/pages/login/LoginPage.jsx
@@ -3,20 +3,33 @@ import Logo from "../../reusable-ui/Logo";
 import LoginForm from "./LoginForm";
 
 export default function LoginPage() {
-  //render
   return (
     <LoginPageSytyled>
       <Logo />
-      <LoginForm />;
+      <LoginForm />
     </LoginPageSytyled>
   );
 }
 
 const LoginPageSytyled = styled.div`
-  background: red;
   height: 100vh;
   display: flex;
   flex-direction: column;
   justify-content: center;
   align-items: center;
+
+  ::before {
+    content: "";
+    background: url("/images/burger-and-firies-background.jpg")
+      rgba(0, 0, 0, 0.7);
+    background-size: cover;
+    background-position: center;
+    background-blend-mode: darken;
+    position: absolute;
+    top: 0;
+    right: 0;
+    left: 0;
+    bottom: 0;
+    z-index: -1;
+  }
 `;
